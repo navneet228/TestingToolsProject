33e52cc8ac6d32ae11cfd8d61b67425cf1b1e50b
Harpreet8710 <148901217+Harpreet8710@users.noreply.github.com>
2024-07-18 14:54:43 -0400

    tenth commit

---
+++ Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css'; // Import your CSS file
+
+function Footer() {
+    return (
+       
+          
+          
+            <div className="footer">
+                &copy; {new Date().getFullYear()} Health Hub | Rachna and Harpreet Kaur
+            </div>
+      
+    );
+}
+
+export default Footer;
