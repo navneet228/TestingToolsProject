96884d35d8e18373fd03d20959c5d611132620d2
Rachna2030 <rachnaaulakh@GMAIL.COM>
2024-07-18 14:35:37 -0400

    second commit

---
+++ Activities.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Activities() {
+    return (
+        <div className="activities">
+            <h1>Health & Fitness Classes</h1>
+            <p>Join our various health and fitness classes to stay active and healthy!</p>
+            <ul>
+                <li>Yoga Classes</li>
+                <li>Strength Training Workshops</li>
+                <li>Meditation Sessions</li>
+                <li>Nutrition Workshops</li>
+                <li>Weight Management Programs</li>
+               
+            </ul>
+        </div>
+    );
+}
+
+export default Activities;
