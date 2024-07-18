6abff5f11eec6db16cce93d9b81011963b0e872f
Rachna2030 <rachnaaulakh@GMAIL.COM>
2024-07-18 14:34:51 -0400

    first commit

---
+++ About.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import './Ab.css';
+
+
+
+function About(){
+    return(
+        
+        <div className="about">
+            <h1><u>About us</u></h1>
+            
+       <p><b>Our hospital provide an excellent environment for both routine and specialized medical care.<br></br> 
+        We offer many facilities that cater to a wide range of healthcare needs,<br></br> ensuring that patients receive the best possible treatment. 
+        All doctors are highly skilled <br></br>and experienced, often recognized as some of the best in their respective fields,<br></br> providing top-notch medical expertise and compassionate care.
+         Hospital is well-equipped with <br></br>modern medical technologies, including advanced diagnostic tools, surgical equipment, and intensive care units. <br></br>
+         Additionally, we offer a variety of services such as emergency care, outpatient services,<br></br>  and maternity care, all designed to meet the diverse needs of patients. <br></br>
+         Hospital is committed to delivering high-quality healthcare and ensuring patient well-being <br></br>through their extensive facilities and dedicated medical professionals.
+         </b> </p>
+        
+          
+           
+             
+            </div>
+
+    );
+}
+export default About;
